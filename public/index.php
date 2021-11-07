540617bf810951a25dd4164266546f9a40d95e3a
vinh <vinhnv@email>
2021-11-07 15:36:42 +0700

    increase width of each column

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 // init configuration
 define('IS_WIN', 'WIN' === strtoupper(substr(PHP_OS, 0, 3)));
-const STATIC_VERSION = '6.26.1'; // linux manually, windows auto
+const STATIC_VERSION = '6.26.2'; // linux manually, windows auto
 
 // bootstrapping
 include '../bootstrap.php';
