1db17a1450ebb0428e3fedefc640f96b5a2d84b6
vinh <vinhnv@email>
2021-11-30 16:48:08 +0700

    static version

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 // init configuration
 define('IS_WIN', 'WIN' === strtoupper(substr(PHP_OS, 0, 3)));
 // linux manually, windows auto
-define('STATIC_VERSION', IS_WIN ? date('Y-m-d H:i:s') : @file_get_contents('status_version'));
+define('STATIC_VERSION', IS_WIN ? date('Y-m-d H:i:s') : file_get_contents(dirname(__FILE__) . '/static_version'));
 
 // bootstrapping
 include '../bootstrap.php';
